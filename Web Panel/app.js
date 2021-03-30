5021c57efc691d38c4e7286511247a13b86ccee8
Prateek Chaubey <41151614+prateek-chaubey@users.noreply.github.com>
2021-03-30 19:16:46 +0530

    Add files via upload

---
+++ Web Panel/app.js
@@ -0,0 +1,18 @@
+///Edit Under This only
+
+var firebaseConfig = {
+  apiKey: "api-key",
+  authDomain: "project-id.firebaseapp.com",
+  databaseURL: "https://project-id.firebaseio.com",
+  projectId: "project-id",
+  storageBucket: "project-id.appspot.com",
+  messagingSenderId: "sender-id",
+  appId: "app-id",
+  measurementId: "G-measurement-id",
+};
+
+     
+ 
+////Do Not Edit Below This
+
+firebase.initializeApp(firebaseConfig);
