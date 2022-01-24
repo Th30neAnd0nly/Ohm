6a218c7d0e4b81d403530279047362db39163a7d
iamsamir090 <81808632+iamsamir090@users.noreply.github.com>
2022-01-25 00:02:38 +0530

    Update app.js

--- Web Panel/app.js
+++ Web Panel/app.js
@@ -2,13 +2,13 @@
 
   
     var firebaseConfig = {
-    apiKey: "API KEY",
-    authDomain: "AUTH DOMAIN",
-    databaseURL: "DATABSE URL",
-    projectId: "PROJECT ID",
-    storageBucket: "BUCKET URL",
-    messagingSenderId: "SENDER ID",
-    appId: "APP ID"
+    apiKey: "AIzaSyAxLtt_4Nc4GIHW66VQFoi1lOk9YIoirHI",
+    authDomain: "whats-92218.firebaseapp.com",
+    databaseURL: "https://whats-92218-default-rtdb.firebaseio.com",
+    projectId: "whats-92218",
+    storageBucket: "whats-92218.appspot.com",
+    messagingSenderId: "397720262504",
+    appId: "1:397720262504:web:bf3f4d3f45ae89449b28dd"
   }; 
  
 ////Do Not Edit Below This
